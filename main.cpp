cce135f608d386280798c613f20d914a0b9e979f
Nihilus89 <v.bimpikas@gmail.com>
2016-02-20 14:30:51 +0100

    Rigid transform is now working as it should (tested side-by-side with MATLAB), and also a separate function

--- main.cpp
+++ main.cpp
@@ -15,12 +15,14 @@ int fiducial[3][2] = {
 
 
 int locateFiducial(int target_x, int target_y, int* fiducial_x, int* fiducial_y, int* fiducial_r);
+void rigidTransform(cv::Mat A, cv::Mat B, cv::Mat& R, cv::Mat& t);
 
 using namespace cv;
 using namespace std;
 
 int main()
 {
+
     int fidNum, i;
     fidNum = sizeof(fiducial)/sizeof(fiducial[0]);
     for (i = 0; i<fidNum; i++)
@@ -97,4 +99,93 @@ int locateFiducial(int target_x, int target_y, int* fiducial_x, int* fiducial_y,
         }
     }
     return 0;
+}
+
+/*  NAME:               void rigidTransform(cv::Mat A, cv::Mat B, cv::Mat& R, cv::Mat& t)
+ *
+ *  DESCRIPTION:        This function finds the optimal Rigid/Euclidean transform
+ *
+ *  INPUTS:
+ *  -cv::Mat A          The fiducial coordinates read from the BOM file
+ *
+ *
+ *  -cv::Mat B          The current fiducial coordinates
+ *
+ *
+ *  OUTPUTS:
+ *  -cv::Mat& R         Optimal rotation matrix
+ *
+ *  -cv::Mat& t         Translation matrix
+ */
+
+void rigidTransform(cv::Mat A, cv::Mat B, cv::Mat& R, cv::Mat& t)
+{
+    int N; // Number of points
+    Mat S,U,V; // SVD matrices
+    Mat centroid_A, centroid_APrime,centroid_BPrime, centroid_B, H, temp, trans;
+
+
+    reduce(A,centroid_A, 0, CV_REDUCE_AVG); // MATLAB: centroid_A = mean(A);
+    reduce(B,centroid_B, 0, CV_REDUCE_AVG); // MATLAB: centroid_B = mean(B);
+
+    N = A.rows;                             // MATLAB: N = size(A,1);
+
+    // MATLAB:  H = (A - repmat(centroid_A, N, 1))' * (B - repmat(centroid_B, N, 1));
+    temp = A - repeat(centroid_A, N, 1); // A - repmat(centroid_A, N, 1)
+    transpose(temp, trans); // (A - repmat(centroid_A, N, 1))'
+    H = trans * (B - repeat(centroid_B, N, 1)); //   H = (A - repmat(centroid_A, N, 1))' * (B - repmat(centroid_B, N, 1));
+
+    // MATLAB: [U,S,V] = svd(H);
+    cv::SVD s; // SVD constructor
+    s.compute(H,S,U,V); // Compute SVD
+
+    // We get S as a single column, put the values at a diagonal to match MATLAB's results
+    S = Mat::diag(S);
+
+    // Negate U(:,1) to match MATLAB's results
+    temp = U.col(0);
+    U.col(0) = temp * -1;
+
+    // Negate V(1,:) to match MATLAB's results
+    temp = V.row(0);
+    V.row(0) = temp * -1;
+
+    // MATLAB: R = V*U';
+    transpose(U, trans); // U'
+    R = V * trans; // R = V*U';
+
+    if(determinant(R)<0)
+    {
+        cout << "Reflection detected" << endl;
+        temp = V.col(1); // MATLAB: temp = V(:,2);
+        V.col(1) = temp * -1; // MATLAB: V(:,2) = temp * -1;
+        R = V * trans; // MATLAB: R = V*U';
+    }
+    //cout << "R = "<< endl << " "  << R << endl << endl;
+
+    // MATLAB: t = -R*centroid_A' + centroid_B'
+    transpose(centroid_A, centroid_APrime); // centroid_A'
+    transpose(centroid_B, centroid_BPrime); // centroid_B'
+    t = -R * centroid_APrime + centroid_BPrime; // t = -R*centroid_A' + centroid_B'
+    //cout << "t = "<< endl << " "  << t << endl << endl;
+
+
+
+
+
+    float data3[1][2] = {32, 12};
+    Mat test, test_tr, A2;
+    test = Mat(1, 2, CV_32F, data3);
+
+    //A2 = (ret_R*A') + repmat(ret_t, 1, n);
+    transpose(A, trans);
+    A2 = (R*trans) + repeat(t, 1, N);
+    //test_tr = (ret_R*test') + repmat(ret_t, 1, 1);
+    transpose(test,trans);
+    test_tr = (R * trans) + repeat(t, 1, 1);
+    transpose(test_tr,trans);
+    cout << "test_tr = "<< endl << " "  << trans << endl << endl;
+
+    transpose(A2, trans);
+    cout << "A2 = "<< endl << " "  << trans << endl << endl;
 }
